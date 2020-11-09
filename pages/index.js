bf95756cce5b879de6adbd5302dad3b1ee8e4d6e
Vandeir Barreiro <vandeir.barreiro@portoseguro.com.br>
2020-11-09 19:02:47 -0300

    change home

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import {useState} from 'react'
 
 function Home(){
     return <div>
-                <h1>Home AAA {process.env.NEXT_PUBLIC_HOME_ENV} </h1>
+                <h1>Home {process.env.NEXT_PUBLIC_HOME_ENV} </h1>
                 <Contador />
             </div>
 }
